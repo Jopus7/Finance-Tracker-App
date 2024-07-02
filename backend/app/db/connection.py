e63fae7c8378805352ca1d42e9b35c2b2bf5bc0d
Michał Jopa <micha.jopa@gmail.com>
2024-07-02 19:54:11 +0200

    create user model in db

--- backend/app/db/connection.py
+++ backend/app/db/connection.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import database_url
+from sqlalchemy.orm import DeclarativeBase
 
 engine = create_engine(database_url())
 session = sessionmaker(bind=engine)
+
+
+class BaseModel(DeclarativeBase):
+    def __repr__(self) -> str:
+        return str(self)
+
 
 def get_db():
     db = session()
@@ -11,3 +18,5 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
