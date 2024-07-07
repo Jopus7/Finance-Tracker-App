e3987ecae96cbc43391f7d4cbe2206965773542b
Przemysław Markiewicz <przemyslaw.markiewicz@u2i.com>
2024-07-07 22:36:17 +0200

    Added Ruff - a tool for formatting and linter

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,8 +6,8 @@ from typing import Optional
 @cache
 def _environment_variable(variable_name: str, default: Optional[str] = None) -> str:
     variable_value = os.environ.get(variable_name, default)
-    
+
     if not variable_value:
         raise Exception(f"Variable {variable_name} not found")
-    
+
     return variable_value
