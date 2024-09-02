937ce23f0afe25fdf6465444b19b43bdaea4f8b4
Przemysław Markiewicz <przemyslaw.markiewicz@u2i.com>
2024-09-02 21:02:38 +0200

    Fixed test db connection

--- backend/tests/config/db.py
+++ backend/tests/config/db.py
@@ -1,5 +1,3 @@
-import urllib.parse
-
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, text
@@ -16,49 +14,46 @@ def test_database_name() -> str:
     return database_name() + "_test"
 
 
-def test_database_url() -> str:
-    return "postgresql+pg8000://{user}:{password}@{host}:{port}/{database}".format(
-        host=database_host(),
-        port=database_port(),
-        database=test_database_name(),
-        user=database_user(),
-        password=urllib.parse.quote(database_password()),
-    )
-
-
-def master_database_url() -> str:
-    return "postgresql+pg8000://{user}:{password}@{host}:{port}/postgres".format(
-        host=database_host(),
-        port=database_port(),
-        user=database_user(),
-        password=urllib.parse.quote(database_password()),
-    )
-
-
 @pytest.fixture(scope="session")
 def create_test_database():
-    master_engine = create_engine(master_database_url(), isolation_level="AUTOCOMMIT")
-    with master_engine.connect() as connection:
+    engine = create_engine(
+        "postgresql+pg8000://{user}:{password}@{host}:{port}".format(
+            user=database_user(),
+            password=database_password(),
+            host=database_host(),
+            port=database_port(),
+        ),
+        isolation_level="AUTOCOMMIT",
+    )
+    with engine.connect() as connection:
         connection.execute(text("DROP DATABASE IF EXISTS {db_name}".format(db_name=test_database_name())))
         connection.execute(text("CREATE DATABASE {db_name}".format(db_name=test_database_name())))
-    master_engine.dispose()
+    engine.dispose()
 
 
 @pytest.fixture(scope="session")
-def engine(create_test_database):
-    return create_engine(test_database_url(), isolation_level="AUTOCOMMIT")
+def database_engine(create_test_database):
+    return create_engine(
+        "postgresql+pg8000://{user}:{password}@{host}:{port}/{database}".format(
+            user="postgres",
+            password="password",
+            host=database_host(),
+            port=database_port(),
+            database=test_database_name(),
+        )
+    )
 
 
 @pytest.fixture(scope="session")
-def connection(engine):
-    with engine.connect() as connection:
+def connection(database_engine):
+    with database_engine.connect() as connection:
         with connection.begin():
-            BaseModel.metadata.create_all(bind=engine)
+            BaseModel.metadata.create_all(bind=database_engine)
         yield connection
     connection.close()
 
 
-@pytest.fixture()
+@pytest.fixture
 def session(connection):
     transaction = connection.begin()
 
@@ -70,10 +65,9 @@ def session(connection):
     finally:
         db.close()
         transaction.rollback()
-        connection.close()
 
 
-@pytest.fixture()
+@pytest.fixture
 def client(session):
     def override_get_db():
         yield session
