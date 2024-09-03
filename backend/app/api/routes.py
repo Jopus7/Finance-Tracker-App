e0d741103d4be58c3f20ce7501e64c13b5dda79e
Michał Jopa <micha.jopa@gmail.com>
2024-09-03 17:43:14 +0200

    auth with token

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, FastAPI
 
 from app.users.routes import user_router
+from app.auth.routes import auth_router
 
 app = FastAPI()
 
 api_router = APIRouter()
-api_router.include_router(user_router, prefix="/users")
-
 app.include_router(api_router, prefix="/api")
+
+api_router.include_router(user_router, prefix="/users")
+api_router.include_router(auth_router, prefix="/auth")
--- backend/app/auth/helpers.py
+++ backend/app/auth/helpers.py
@@ -1,22 +1,23 @@
 from pydantic import BaseModel
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
-import jwt
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, Depends, status, APIRouter
+from app.users.models import User
+from app.db.connection import db_session
+from app.auth.schemas import Token, TokenData
+
 
 SECRET_KEY = "test_secret"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-    
-class TokenData(BaseModel):
-    email: str | None = None
-
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/token")
 
     
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -26,6 +27,17 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def get_password_hash(password: str) ->str:
     return pwd_context.hash(password)
 
+def get_user(dbs: Session, email: str) -> User:
+    return dbs.query(User).filter(User.email==email).first()
+
+
+def authenticate_user(dbs: Session, email: str, password: str) -> User:
+    user = get_user(dbs, email)
+    if not user:
+        return None
+    if not verify_password(password, user.password):
+        return None
+    return user
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
@@ -37,3 +49,24 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+
+async def get_current_user(dbs: Session = Depends(db_session), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = TokenData(email=email)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(dbs, email=token_data.email)
+    if user is None:
+        raise credentials_exception
+    return user
